cb44d9b8126a77c4bec52217d1d79c246e479eb4
Mr-Johny-Blaska <121683366+Mr-Johny-Blaska@users.noreply.github.com>
2023-07-16 16:04:54 +0200

    background animation color fix

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ function App() {
         }}
       >
         <Canvas shadows camera={{ position: [0, 3, 10], fov: 42 }}>
-          <color attach="background" args={["#e6e7ff"]} />
+          <color attach="background" args={["#b6defa"]} />
           <ScrollControls pages={4} damping={0.1}>
             <ScrollManager section={section} onSectionChange={setSection} />
             <Scroll>
